79956085cb5c5480843119697d8c88d47923b458
junqiangchen <1207173174@qq.com>
2019-07-11 14:55:44 +0800

    Update util.py

--- LUNA16Challege/Vnet/util.py
+++ LUNA16Challege/Vnet/util.py
@@ -3,19 +3,15 @@ from glob import glob
 
 
 # save .npy file path to csv
-def save_npy2csv(path, name, labelnum=1, label=False):
-    if label:
-        out = open(name, 'w')
-        file_list = glob(path + "*.npy")
-        out.writelines("index" + "," + "filename" + "\n")
-        for index in range(len(file_list)):
-            out.writelines(str(labelnum) + "," + file_list[index] + "\n")
-    else:
-        out = open(name, 'w')
-        file_list = glob(path + "*.npy")
-        out.writelines("filename" + "\n")
-        for index in range(len(file_list)):
-            out.writelines(file_list[index] + "\n")
+def save_npy2csv(path, name, labelnum=1):
+    """
+    this is for classify,save label+filepath into csv
+    """
+    out = open(name, 'w')
+    file_list = glob(path + "*.npy")
+    out.writelines("index" + "," + "filename" + "\n")
+    for index in range(len(file_list)):
+        out.writelines(str(labelnum) + "," + file_list[index] + "\n")
 
 
-save_npy2csv("G:\Data\LIDC\LUNA16\classsification\\1_aug\\", "nodel_positive.csv", 1, True)
+#save_npy2csv("G:\Data\LIDC\LUNA16\classsification\\1_aug\\", "nodel_positive.csv", 1)
