f5fcb6129671d58585382b9a92cb9babe3f41008
junqiangchen <1207173174@qq.com>
2020-04-17 14:25:08 +0800

    Update LUNA_node_extraction.py

--- LUNA16Challege/dataprocess/LUNA_node_extraction.py
+++ LUNA16Challege/dataprocess/LUNA_node_extraction.py
@@ -74,11 +74,16 @@ def get_node_classify():
         luna_subset_path = luna_path + "subset" + str(subsetindex) + "/"
         output_path = "G:\Data\LIDC\LUNA16\classsification/"
         file_list = glob(luna_subset_path + "*.mhd")
+        
+        file_list_path=[]
+        for i in range(len(file_list)):
+            file_list_path.append(file_list[i][0:-4])
+        
 
         # The locations of the nodes
         luna_csv_path = "G:\Data\LIDC\LUNA16"
         df_node = pd.read_csv(luna_csv_path + "/CSVFILES/" + "candidates.csv")
-        df_node["file"] = df_node["seriesuid"].map(lambda file_name: get_filename(file_list, file_name))
+        df_node["file"] = df_node["seriesuid"].map(lambda file_name: get_filename(file_list_path, file_name))
         df_node = df_node.dropna()
         # Looping over the image files
         for fcount, img_file in enumerate(tqdm(file_list)):
@@ -86,6 +91,7 @@ def get_node_classify():
             mini_df = df_node[df_node["file"] == img_file]
             # some files may not have a nodule--skipping those
             if mini_df.shape[0] > 0:
+                img_file=img_file+".mhd"
                 # load the data once
                 itk_img = load_itkfilewithtrucation(img_file, 600, -1000)
                 img_array = sitk.GetArrayFromImage(itk_img)
