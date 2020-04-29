5b6793aba7cf9c1ab9ab0b7fc904ccee6a493ae3
junqiangchen <1207173174@qq.com>
2020-04-29 18:37:28 +0800

    Update vnet3d_predict.py

--- LUNA16Challege/vnet3d_predict.py
+++ LUNA16Challege/vnet3d_predict.py
@@ -1,7 +1,7 @@
 import os
 
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 from tensorflow.python.client import device_lib
 
 print(device_lib.list_local_devices())
